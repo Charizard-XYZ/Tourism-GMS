97e47f12002a00ec641b1fb5e58dd2360feea42f
Abhishek Kumar <abhishekkrsharma159@gmail.com>
2026-09-06 21:55:01 +0530

    Fix Vercel serverless module loading

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,8 @@
-﻿import app from '../server/src/index';
-export default app;
+// Express application instance
+const app = require('../server/src/index');
+
+// Handle both ES module default export and CommonJS module.exports
+const handler = app.default || app;
+
+module.exports = handler;
+module.exports.default = handler;
