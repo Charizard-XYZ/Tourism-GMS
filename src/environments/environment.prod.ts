ddf1d24980743ac2937556fda0e470d3f87c63ef
Abhishek Kumar <abhishekkrsharma159@gmail.com>
2026-09-07 12:42:27 +0530

    Connect production API to Render backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5000/api',
+ apiUrl: 'https://tourism-gms-backend.onrender.com/api',
   firebase: {
     apiKey: 'AIzaSyCaZIXAbEaQNqUUawZHbt8S2HaVq-o23Zs',
     authDomain: 'tourism-gms.firebaseapp.com',
